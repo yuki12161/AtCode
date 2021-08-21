9ec28a66d557fb85c51b1a49ba59300f5ce23d63
yuki12161 <yuki_sato1216@yahoo.co.jp>
2021-08-21 23:12:41 +0900

    atcode

--- 20210814_A.py
+++ 20210814_A.py
@@ -1,8 +1,10 @@
 N = int(input())
 
+an = '8'
+
 if 1 <= N < 126:
-    print('4')
-elif 126 <= N <212:
-    print('6')
-elif 212 <= N < 215:
-    print('8')
+    ans = '4'
+elif 126 <= N < 212:
+    ans = '6'
+
+print(ans)
